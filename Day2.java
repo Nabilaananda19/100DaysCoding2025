ffa69e5d68315f56e6529ddb9179f90c1db68bbe
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-09 17:01:04 +0700

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+public class JavaDay2 {
+
+    public static void main(String[] args) {
+        String nama;
+        int umur;
+        float tinggi;
+        
+        nama = "Nabila ananda salsabila";
+        umur = 18;
+        tinggi = 156;
+        
+        System.out.print("hallo guys,");
+        System.out.println(" kenalin");
+        System.out.printf("nama saya: %s umur:  %d tinggi:  %.1f", nama, umur, tinggi);
+    }
+    
+}

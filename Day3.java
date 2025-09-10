d4aff557832dfa8a7a410c164d929c89e587211f
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-10 10:32:45 +0700

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,15 @@
+public class JavaDay3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Komentar untuk satu baris
+        /*Komentar lebih dari satu baris*/
+        
+        //Menampilkan pesan untuk mencetak nama kampus
+        System.out.println("Universitas Sulawesi Barat");
+        
+    }
+    
+}

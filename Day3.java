ae30b4a10db5da69f3a02cd947af38d47e06499f
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-10 19:07:38 +0700

    Day3.java

--- Day3.java
+++ Day3.java
@@ -7,8 +7,20 @@ public class JavaDay3 {
         //Komentar untuk satu baris
         /*Komentar lebih dari satu baris*/
         
-        //Menampilkan pesan untuk mencetak nama kampus
-        System.out.println("Universitas Sulawesi Barat");
+        //Menampilkan pesan untuk nama 
+        //Baris ini akan menampilkan nama panggilan saya
+        System.out.println("Nabila");
+        
+        /*
+        Baris ini digunakan untuk menampilkan sapaan 
+        dan nama kampus
+        */
+        String namakampus;
+        namakampus = "Universitas Sulawesi barat";
+        
+        //Menampilkan pesan sapaan
+        System.out.println("Halo Namakampus saya adalah : " + namakampus);
+        
         
     }
     

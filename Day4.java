aa6e6c0981415bdc704829d1cdfae048e1cb8522
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-11 18:41:52 +0700

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package javaday4;
+
+public class Javaday4 {
+    public static void main(String[] args) {
+        // \n digunakan untuk membuat baris baru
+        System.out.println("Hai kenalin aku Nabila.\nSenang berkenalan denganmu.");
+        
+        // \t untuk tab atau spasi
+        System.out.println("Nama teman:\tZulfiah\tTitin\tDida\tDina\tElsa");
+        
+        /* \\Backlash untuk menampilkan karakter
+        */
+        System.out.println("File disimpan di: C:\\user\\nama user\\docx");
+        
+        // \"untuk menampilkan tanda kutip gaanda
+        System.out.println("kakak berkata,\"Ayo belajar\"");
+        
+        
+     }
+    
+}

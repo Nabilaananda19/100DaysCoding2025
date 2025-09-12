9583545b76dfdcb2e02faf4f966e14b485aa2130
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-12 19:41:43 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package javaday5;
+
+public class Javaday5 {
+    public static void main(String[] args) {
+        byte umur = 18;
+        short tahunLahirMama = 1982;
+        long jarakKostkeKampus = 8500L;
+        int ukuransepatu = 39;
+          System.out.println("umur = " + umur);
+          System.out.println("tahunLahirMama = " + tahunLahirMama);
+          System.out.println("jarakKostkeKampus = " + jarakKostkeKampus);
+          System.out.println("ukuransepatu = " + ukuransepatu);   
+    }
+    
+}

cf3aa4b6baf9ea1f827216d6df046cd0a9e44f69
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-13 19:57:55 +0700

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package javaday6;
+
+public class JavaDay6 {
+    public static void main(String[] args) {
+        float beratbadan = 42.1f ;
+        System.out.println("berat badan saya " + beratbadan + " kg"  );
+        
+        double suhu = 36.4;
+        System.out.println("suhu = " + suhu);
+    }
+    
+}

0c92af8a5bdc9c3d5e5c0a85724d16d912a2cd5b
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-14 19:23:49 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,16 @@
+package day7java;
+
+public class Day7java {
+    public static void main(String[] args) {
+        char ukuranbaju = 'M';
+        System.out.println("ukuran baju = " + ukuranbaju);
+        
+        boolean anakKedua = true;
+        System.out.println("anak kedua");
+        
+        boolean sukaSayur = false;
+        System.out.println("suka sayur");
+         
+    }
+    
+}

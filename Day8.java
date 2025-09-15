b88de8597a1c838bf9acded60abbb019378ceb6c
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-15 20:16:26 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,13 @@
+package day8java;
+
+public class Day8java {
+    public static void main(String[] args) {
+        String perkenalan = ("nama saya Nabila ananda salsabila");
+        System.out.println("perkenalan = " + perkenalan);
+        
+        String alasanMemilihUnsulbar = ("karena rekomendasi dari orang tua dan keluarga");
+        System.out.println("alasan Memilih Unsulbar = " + alasanMemilihUnsulbar);
+        
+         }
+    
+}

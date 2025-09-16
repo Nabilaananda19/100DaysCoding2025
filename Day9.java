ad6995803210ae0f9e53f7d3ae2c62e1425721e9
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-16 20:43:02 +0700

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,22 @@
+package day9java;
+
+public class Day9java {
+    public static void main(String[] args) {
+        //Deklarasi
+        String nama;
+        double tinggi;
+        
+        //Inisialisasi      
+        nama = "Nabila Ananda";
+        tinggi = 155.8;
+        
+        //Update Variabel
+        nama = "Nabila Ananda Salsabila";
+        System.out.println("nama = " + nama);
+        tinggi = 156.4;
+        System.out.println("tinggi = " + tinggi);
+        
+        
+    }
+    
+}

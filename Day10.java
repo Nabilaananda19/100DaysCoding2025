3b70bc163bd5fdb653a5364df9caee8f5f68a7e0
Nabila Ananda Salsabila <nana.bilasalsabila07@gmail.com>
2025-09-17 21:42:08 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,26 @@
+package day10java;
+
+public class Day10java {
+    public static void main(String[] args) {
+        final String nama = "Nabila Ananda";
+        final short a = 25;
+        final byte u = 18;
+        final double t = 150.4;
+        final char jenis_kelamin = 'p';
+        final boolean n = true;
+        final int j = 15;
+        final float b = 40.7f;
+        final long v = 4444;
+        
+         System.out.println("nama = " + nama);
+         System.out.println("a = " + a);
+         System.out.println("u = " + u);
+         System.out.println("t = " + t);
+         System.out.println("jenis kelamin = " + jenis_kelamin);
+         System.out.println("n = " + n);
+         System.out.println("j = " + j);
+         System.out.println("b = " + b);
+         System.out.println("v = " + v);
+    }
+    
+}
